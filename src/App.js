8a1b51855056a8b4d2f24e459764ab480d86bcff
priyan <45042030+priyan1995@users.noreply.github.com>
2021-09-04 21:13:37 +0530

    table sorting

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch, Link  } from 'react-router-dom';
 import { BasicTable } from './components/basicTable';
+import { BasicSortTable } from './components/sortTable';
 
 function App() {
   return (
-    <div className="App">
-<BasicTable />
-    </div>
+
+    <Router >
+      <div className="App">
+
+        <Link to="/sort-table">Sorted Table</Link>
+        <span> | </span>
+        <Link to="/">Table</Link>
+
+        <Switch>
+          <Route exact path="/">
+            <BasicTable />
+          </Route>
+          <Route path="/sort-table">
+            <BasicSortTable />
+          </Route>
+        </Switch>
+
+      </div>
+    </Router>
   );
 }
 
