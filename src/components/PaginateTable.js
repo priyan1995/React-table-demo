2e18fb0081e00c6acd41239c1979f4a8214025f1
priyan <45042030+priyan1995@users.noreply.github.com>
2021-09-05 22:48:01 +0530

    pagination next button added

--- src/components/PaginateTable.js
+++ src/components/PaginateTable.js
@@ -34,6 +34,8 @@ export const PaginateTable = () => {
         canNextPage,
         canPreviousPage,
         pageOptions,
+        gotoPage,
+        pageCount,
         prepareRow
     } = tableInstance
 
@@ -89,17 +91,34 @@ export const PaginateTable = () => {
                     }
                 </tbody>
 
-             
+
 
 
             </table>
-            Page { ' ' }
-            <span>
-               <strong> {pageIndex + 1} of { pageOptions.length } </strong>
-            </span> { ' ' }
-            <button onClick={() => previousPage()} disabled={!canPreviousPage}>Previous</button>
-            <button onClick={() => nextPage()} disabled={!canNextPage}>Next</button>
+            <div>
+                Page {' '}
+                <span>
+                    <strong> {pageIndex + 1} of {pageOptions.length} </strong>
+                </span> {' '}
 
+                <span>
+                    | Go to page: {' '}
+                    <input
+                        type='number'
+                        defaultValue={pageIndex + 1}
+                        onChange={(e) => {
+                            const pageNumber = e.target.value ? Number(e.target.value) - 1 : 0
+                            gotoPage(pageNumber)
+                        }}
+                    style={{ width: '50px' }}/>
+                </span>
+
+
+                <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>{'<<'}</button>
+                <button onClick={() => previousPage()} disabled={!canPreviousPage}>Previous</button>
+                <button onClick={() => nextPage()} disabled={!canNextPage}>Next</button>
+                <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>{'>>'}</button>
+            </div>
 
         </div>
     )
